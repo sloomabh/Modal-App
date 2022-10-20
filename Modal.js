346a0b3cd9479f0e6729de3b54a40bf1bf4e24fd
salimbh <sloomabbenhalima95@gmail.com>
2022-10-20 10:54:46 +0900

    first commit

---
+++ Modal.js
@@ -0,0 +1,33 @@
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModel = document.querySelector('.close-modal');
+const btnOpenModel = document.querySelectorAll('.show-modal');
+
+console.log(btnOpenModel);
+
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpenModel.length; i++)
+  btnOpenModel[i].addEventListener('click', openModal);
+
+btnCloseModel.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+//we close the window when we press on "Escape' key"
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
